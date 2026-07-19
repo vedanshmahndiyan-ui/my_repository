028427164a96eae3481706c8553ed60c281cb5f9
Vedansh Singh <vedanshmahndiyan@gmail.com>
2026-07-19 15:02:02 +0530

    completed week 1 task

---
+++ to-do_list_app/to-do_app.py
@@ -0,0 +1,8 @@
+user_name = input("Enter your name: ")
+print(f"Hello, {user_name}! Welcome to the To-Do List App.")
+
+task1 = input("Enter your first task: ")
+print(f"Task 1: {task1} has been added to your to-do list.")
+
+print("Here are your current tasks:")
+print(f"1. {task1}")
